4df444e52f42d34a513e4cc9b56e935aa1f00310
utsav-cyber <75036592+utsav-cyber@users.noreply.github.com>
2021-11-28 19:35:02 +0530

    Delete Util.java

--- src/Util.java
+++
@@ -1,5 +0,0 @@
-public class Util {
-	public static void print(Player player, String msg) {
-		System.out.println("[Turn " + (player.getTotalWalk() + 1) + "] [" + player.getCurrentPosition() + "] [$" + player.getMoney().getMoney() + "] " + msg);
-	}
-}
